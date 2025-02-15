f8b90bf0a9d5a270f08dfe15bab930c4e0d52428
Sunil <sunil.maurya1@indiamart.com>
2025-02-15 17:54:40 +0530

    bug fix

--- src/pages/UsersPage.jsx
+++ src/pages/UsersPage.jsx
@@ -5,8 +5,8 @@ import Header from "../components/common/Header";
 import StatCard from "../components/common/StatCard";
 import UsersTable from "../components/users/UsersTable";
 import UserGrowthChart from "../components/users/UserGrowthChart";
-//import UserActivityHeatmap from "../components/users/UserActivityHeatmap";
 import UserDemographicsChart from "../components/users/UserDemographicsChart";
+import UserActivityHeatmap from "../components/users/UserActivityHeatMap2";
 
 const userStats = {
 	totalUsers: 152845,
@@ -49,7 +49,7 @@ const UsersPage = () => {
 				{/* USER CHARTS */}
 				<div className='grid grid-cols-1 lg:grid-cols-2 gap-6 mt-8'>
 					<UserGrowthChart />
-					{/* <UserActivityHeatmap /> */}
+					<UserActivityHeatmap />
 					<UserDemographicsChart />
 				</div>
 			</main>
